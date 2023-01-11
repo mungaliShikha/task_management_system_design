d79fb20df410667e471fa021078c5da73b8c2fa1
subhra <subhra.rai@antino.io>
2023-01-11 15:04:36 +0530

    code update

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -6,12 +6,23 @@ const { ErrorCode, SuccessCode } = require("../helper/statusCode");
 const { compareHash, generateToken } = require("../helper/commonFunction");
 const helper = require("../helper/commonResponseHandler");
 
-module.exports.login = catchAsync(async (req, res) => {
-  const { email, password } = req.body;
-  const loggedInUser = await User.findOne({ email });
-  if (!loggedInUser || !compareHash(password, loggedInUser.password)) {
-    throw new appError(ErrorMessage.EMAIL_NOT_REGISTERED, ErrorCode.NOT_FOUND);
-  } else {
-    helper.sendResponseWithData(res, SuccessCode.SUCCESS,SuccessMessage.LOGIN_SUCCESS, loggedInUser, generateToken({ email }));
-  }
-});
+
+
+
+module.exports = {
+  login: async (req, res) => {
+    const { email, password } = req.body;
+    const loggedInUser = await User.findOne({ email });
+    if (!loggedInUser || !compareHash(password, loggedInUser.password)) {
+      throw new appError(ErrorMessage.EMAIL_NOT_REGISTERED, ErrorCode.NOT_FOUND);
+    } else {
+      helper.sendResponseWithData(res, SuccessCode.SUCCESS, SuccessMessage.LOGIN_SUCCESS, loggedInUser, generateToken({ email }));
+    }
+  },
+  otpVerify: async (req, res) => {
+    // bussiness logic here,
+  },
+
+  resendOtp: async (req, res) => {
+  },
+}
