1981de9c05f8c75bb091694a5a9538852a024ac2
shikhamungali <shikha1081998@gmail.com>
2023-01-12 12:45:39 +0530

    updated code

--- routes/adminRoute/admin.route.js
+++ routes/adminRoute/admin.route.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const {login,forgetPassword,resetPassword,updateAdmin,getAdmin} = require("../../controllers/admin.controller");
+const {loginDeveloper,addDeveloper,listDeveloper,viewDeveloper} = require("../../controllers/user.controller");
+const { loginManager, createManager } = require("../../controllers/admin.controller");
 const auth = require('../../middleware/auth');
 const { upload } = require("../../services/aws/aws");
 
@@ -11,8 +13,21 @@ router.post('/resetPassword/:userId/:token',resetPassword)
 router.put('/updateAdmin/:userId',upload.single("profile_image"),updateAdmin)
 router.get('/getAdminDetails/:userId',getAdmin)
 
+/// admin can create manager
+router.post("/loginManager", loginManager);
+router.post("/createManager", createManager);
 
 
+
+router.post('/addDeveloper', addDeveloper)
+router.post('/loginDeveloper', loginDeveloper);
+router.get('/listDeveloper', listDeveloper)
+router.get('/viewDeveloper', viewDeveloper)
+
+
+
+
+module.exports = router;
 
 
 
