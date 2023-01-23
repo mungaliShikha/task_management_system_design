75ffcc012db1167ab447bb8b151d30e0cdb483d2
nitin <nitin.s@antino.io>
2023-01-23 09:58:37 +0530

    made changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,53 +10,69 @@ const {
   generateHash,
 } = require("../helper/commonFunction");
 const helper = require("../helper/commonResponseHandler");
+const { sendMail, sendMailNotify } = require("../utils/nodeMailer/nodemailer");
 const {
-  sendMail,
-  sendMailNotify,
-} = require("../utils/nodeMailer/nodemailer");
-const {
-  getOneUser, getAllUser, getUserById, getUserAndUpdate, getOneToken, createUser
-} = require("../services/user.service")
+  getOneUser,
+  getAllUser,
+  getUserById,
+  getUserAndUpdate,
+  getOneToken,
+  createUser,
+} = require("../services/user.service");
 
-const enums = require("../helper/enum/enums")
+const enums = require("../helper/enum/enums");
 
-const {subjects,messages}=require('../helper/commonFunction');
+const { subjects, messages } = require("../helper/commonFunction");
 
 module.exports = {
   //******************************** common login api for manager developer admin ************************** */
- login: catchAsync(async (req, res) => {
-  const { email, password } = req.body;
-  const loggedInUser = await getOneUser({ email ,role:{ $in: [ enums.declaredEnum.role.MANAGER, enums.declaredEnum.role.DEVELOPER, enums.declaredEnum.role.ADMIN ] }});
-  if (!loggedInUser || !compareHash(password, loggedInUser.password)) {
-    throw new appError(
-      ErrorMessage.EMAIL_NOT_REGISTERED,
-      ErrorCode.NOT_FOUND
-    );
-  } else {
-    let token = generateToken({ id: loggedInUser._id });
-    let finalRes = {
-      email: email,
-      mobile_number: loggedInUser.mobile_number,
-      role: loggedInUser.role,
-      first_name: loggedInUser.first_name,
-      last_name: loggedInUser.last_name,
-      token: token,
-    };
-    helper.sendResponseWithData(
-      res,
-      SuccessCode.SUCCESS,
-      SuccessMessage.LOGIN_SUCCESS,
-      finalRes
-    );
-  }
-}),
+  login: catchAsync(async (req, res) => {
+    const { email, password } = req.body;
+    const loggedInUser = await getOneUser({
+      email,
+      role: {
+        $in: [
+          enums.declaredEnum.role.MANAGER,
+          enums.declaredEnum.role.DEVELOPER,
+          enums.declaredEnum.role.ADMIN,
+        ],
+      },
+    });
+    if (!loggedInUser || !compareHash(password, loggedInUser.password)) {
+      throw new appError(
+        ErrorMessage.EMAIL_NOT_REGISTERED,
+        ErrorCode.NOT_FOUND
+      );
+    } else {
+      let token = generateToken({ id: loggedInUser._id });
+      let finalRes = {
+        email: email,
+        mobile_number: loggedInUser.mobile_number,
+        role: loggedInUser.role,
+        first_name: loggedInUser.first_name,
+        last_name: loggedInUser.last_name,
+        token: token,
+      };
+      helper.sendResponseWithData(
+        res,
+        SuccessCode.SUCCESS,
+        SuccessMessage.LOGIN_SUCCESS,
+        finalRes
+      );
+    }
+  }),
 
   // *********************************************** get profile for Manager,Developer *******************************
 
   getProfile: catchAsync(async (req, res) => {
     const tokenAuth = await getOneUser({
       _id: req.userId,
-      role: { $in: [enums.declaredEnum.role.DEVELOPER, enums.declaredEnum.role.MANAGER] },
+      role: {
+        $in: [
+          enums.declaredEnum.role.DEVELOPER,
+          enums.declaredEnum.role.MANAGER,
+        ],
+      },
     });
     if (!tokenAuth) {
       throw new appError(ErrorMessage.DATA_NOT_FOUND, ErrorCode.NOT_FOUND);
@@ -68,6 +84,7 @@ module.exports = {
       SuccessMessage.DATA_FOUND
     );
   }),
+
   // *********************************************** update Profile for Manager,Developer *******************************
 
   updateProfile: async (req, res) => {
@@ -75,7 +92,12 @@ module.exports = {
       let payload = req.body;
       const tokenAuth = await getOneUser({
         _id: req.userId,
-        role: { $in: [enums.declaredEnum.role.DEVELOPER, enums.declaredEnum.role.MANAGER] },
+        role: {
+          $in: [
+            enums.declaredEnum.role.DEVELOPER,
+            enums.declaredEnum.role.MANAGER,
+          ],
+        },
       });
       if (!tokenAuth)
         helper.commonResponse(
@@ -86,12 +108,11 @@ module.exports = {
       if (req.files) {
         payload["profile_pic"] = req.files[0].location;
       }
-      let updateRes = await getUserAndUpdate
-        (
-          { _id: tokenAuth._id },
-          { $set: payload },
-          { new: true }
-        );
+      let updateRes = await getUserAndUpdate(
+        { _id: tokenAuth._id },
+        { $set: payload },
+        { new: true }
+      );
       helper.commonResponse(
         res,
         SuccessCode.SUCCESS,
@@ -112,7 +133,10 @@ module.exports = {
   addDeveloper: catchAsync(async (req, res) => {
     const payload = req.body;
     const { first_name, last_name, email, mobile_number } = payload;
-    const userAuth = await getOneUser({ _id: req.userId, role: enums.declaredEnum.role.MANAGER });
+    const userAuth = await getOneUser({
+      _id: req.userId,
+      role: enums.declaredEnum.role.MANAGER,
+    });
     if (!userAuth) {
       throw new appError(ErrorMessage.NOT_AUTHORISED, ErrorCode.NOT_FOUND);
     }
@@ -127,7 +151,12 @@ module.exports = {
     payload["role"] = "Developer";
     const createDeveloper = await User.create(payload);
 
-    await sendMailNotify(checkManager.email, subjects.DEVELOPER, messages(payload.email,passGen) ,email);
+    await sendMailNotify(
+      checkManager.email,
+      subjects.DEVELOPER,
+      messages(payload.email, passGen),
+      email
+    );
 
     helper.sendResponseWithData(
       res,
@@ -136,7 +165,4 @@ module.exports = {
       createDeveloper
     );
   }),
-  
-
-  
 };
